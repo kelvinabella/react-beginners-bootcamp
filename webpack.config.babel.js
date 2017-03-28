6faa061bd03505c58db02d3a006f26fbb4f22fa0
Adam Goldman <adamgoldman7@gmail.com>
2017-03-29 01:25:52 +0800

    init

---
+++ webpack.config.babel.js
@@ -0,0 +1,36 @@
+import path from 'path'
+import webpack from 'webpack'
+
+const ENV = process.env.NODE_ENV || 'production'
+const isProd = ENV === 'production'
+
+export default {
+  entry: [
+    'react-hot-loader/patch',
+    './src/client',
+  ],
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader', include: /src/ },
+    ],
+  },
+  devtool: isProd ? false : 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: 8000,
+    hot: true,
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+}
